09fe43e3314eb1a604438f3cdc6eea908f1a9bff
chuck-aka-ben <benjamin@thomasnetwork.net>
2013-10-26 13:51:50 -0400

    Implemented Collector

--- subsystems/Collector.c
+++ subsystems/Collector.c
@@ -6,9 +6,11 @@
 
 void setCollectorPosition(int position)
 {
-
+	servo[collectorAngle1] = position;
+	servo[collectorAngle2] = SERVO_MAXIMUM - position;
 }
 
 void setCollectorRoller(bool state)
 {
+	motor[collectorRoller] = (state ? COLLECTOR_ROLLER_MAXIMUM : 0);
 }
